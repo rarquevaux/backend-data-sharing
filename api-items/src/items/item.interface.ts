282cd3d1d1b5849833630d4d30372174bbbdf3f5
Remi Arquevaux <r.arquevaux@gmail.com>
2020-05-03 15:23:18 +0200

    add Share interface

--- api-items/src/items/item.interface.ts
+++ api-items/src/items/item.interface.ts
@@ -2,5 +2,11 @@ export class Item {
     readonly name: string;
     readonly price: number;
     itemHash?: string;
+    shared?: boolean;
+}
+
+export class Share {
+    readonly id: number;
+    readonly recipientId: number;
 }
 
