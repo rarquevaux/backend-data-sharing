6df89d24744fa88f247f70099b2caa4e48584d92
Remi Arquevaux <r.arquevaux@gmail.com>
2020-05-17 18:37:51 +0200

    added share message placeholder

--- ms-data-sharing/src/app.service.ts
+++ ms-data-sharing/src/app.service.ts
@@ -23,13 +23,25 @@ export class AppService {
 
   private logger = new Logger('AppService');
 
-  async share(message: ShareMessage): Promise<string> {
+  async share(message: ShareMessage): Promise<any> {
     this.logger.log("sharing item with recipient...")
 
     //Create onchain privacy group between 1 and 2
     //To do => create on the basis on the recipientId. Do not create if privacy group already exist
     //To do => move this outside of App service (see example)
-    return "not yet implemented"
+    
+    const createPrivacyGroup = () => {
+      const contractOptions = {
+        addresses: [orion.node1.publicKey, orion.node2.publicKey],
+        name: "Privacy Group A",
+        description: "Members of Group A"
+      };
+    web3.eea.createPrivacyGroup(contractOptions).then(result => {
+      this.logger.log(`The privacy group created is:`, result);
+      return result;
+      });
+    };
+
 
   }
 }
